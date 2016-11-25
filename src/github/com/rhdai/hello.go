9758e699f0b00b0a6278b5961ac76ce293fe47f3
rhdai <foxmail_vip@126.com>
2016-11-25 17:25:27 +0800

    helloworld

--- src/github/com/rhdai/hello.go
+++ src/github/com/rhdai/hello.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 )
+
+/*import (
+	"fmt"
+	"os"
+)*/
+/*import "fmt"
+import "os"*/
+/*
+	1.以上2中写法等价
+	2.go语言中认为import的包应该被使用，如果没有被使用会报错
+	3.go语言中不要求语句的结尾必须是；
+
+ */
+
 
 func main() {
 	fmt.Println("hello, world.你好,世界");
  	var a int = 8;
 	var b int = 6;
-	const pi float64 = 3.141592653
-	fmt.Println(pi)
+
+	const pi float64 = 3.141592653;
+	fmt.Println(pi);
 	const p  = 2<<3;
-	fmt.Println(p)
-//	const home  = os.Getenv("path")
+	fmt.Println(p);
+	const home  = os.Getenv("path");
+	fmt.Println(home);
 	a,b = b,a;
 	fmt.Println(a,b);
 	var c,d int = testfunc(a,b);
 	fmt.Println(c,d)
-//	var nickName string;
-//	_,_,nickName := getName();
+	//var nickName string;
+	//_,_,nickName := getName();
 	fmt.Println(getName())
 }
 
